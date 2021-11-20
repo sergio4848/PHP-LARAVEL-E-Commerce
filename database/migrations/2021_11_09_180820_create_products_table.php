12b26a6a1b535900c7e1b8469ec2628217b544db
sergio4848 <44943277+sergio4848@users.noreply.github.com>
2021-11-20 13:59:54 +0300

    Projelerin Veritabani Aciklamasi
    ashdkashdk

---
+++ database/migrations/2021_11_09_180820_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('title', 150);
+            $table->string('keywords')->nullable();
+            $table->string('description')->nullable();
+            $table->string('image', 75)->nullable();
+            $table->integer('category_id')->nullable();
+            $table->integer('user_id')->nullable();
+            $table->float('price')->nullable();
+            $table->integer('quantity')->default(1);
+            $table->integer('minquantity')->default(5);
+            $table->text('detail')->nullable();
+            $table->string('slug', 100)->nullable();
+            $table->string('status', 5)->nullable()->default('False');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
